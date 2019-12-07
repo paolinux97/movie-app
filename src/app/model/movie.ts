6e6d45d9f6a2319756156a2441888731400ee92a
paolinux97 <pchieri97@gmail.com>
2019-12-07 22:50:15 +0100

    Fixed deprecated dependencies

---
+++ src/app/model/movie.ts
@@ -0,0 +1,24 @@
+export class Movie {    // classe Movie non usata dall'applicazione
+    Title: string;
+    Year?: string;
+    Rated?: string;
+    Realesed?: string;
+    Runtime?: string;
+    Genre?: string;
+    Director?: string;
+    Writer?: string;
+    Actors?: string;
+    Plot?: string;
+    Language?: string;
+    Country?: string;
+    Awards?: string;
+    Poster: string;
+    Metascore?: string;
+    imdbRating?: string;
+    imdbVotes?: string;
+    imdbID: string;
+    Type?: string;
+    DVD?: string;
+    BoxOffice?: string;
+    Production?: string;
+}
